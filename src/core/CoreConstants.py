322a0acc01bb2f4887ecfc8d12d0974e2ec7e002
Sergey Osipov <mrfix1033@mail.ru>
2025-05-03 08:38:31 +0300

    beta-12
    Изменения:
    Исправлена ошибка "не найден файл конфигурации" при автозагрузке
    auto_enable_startup по дефолту теперь false

--- src/core/CoreConstants.py
+++ src/core/CoreConstants.py
@@ -1,10 +1,14 @@
+import os
+import sys
+
 import colorama
 
 program_name = "ActionMulticast"
-version = "beta-11"
+version = "beta-12"
 greeting = lambda server_or_client: f"{program_name}{server_or_client}-{version} by mrfix1033 (VK: mrfix1033, Telegram: @mrfix1033)"
 platform_to_extension = {"win32": ".exe"}
 
 
 def init():
+    os.chdir(os.path.dirname(sys.executable))
     colorama.init()
